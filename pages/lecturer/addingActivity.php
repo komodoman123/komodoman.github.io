3403f080c5db6be73fa2b916a9171f5bcca0d09f
Aditya Putra L <adityaputra7997@gmail.com>
2017-11-28 20:12:10 +0700

    Fitur Opsional 3 beres

--- pages/lecturer/addingActivity.php
+++ pages/lecturer/addingActivity.php
@@ -120,70 +120,141 @@ echo $newname;
 			<?php
 				include ('../../layout/sidebar.php');
 			?>
-			<div class="w3-panel w3-grey" style="margin:10px;margin-left:26%">
-			<?php
-				echo "<p class='w3-xlarge w3-text-black'>Adding a new ".$_GET['addType']."</p>";
-				?>
-			</div>
-
-            <fieldset style="margin:10px;margin-left:26%">
-			<form id="usrform"   method="post" enctype="multipart/form-data">
-						<legend>General</legend>
-						<p class="w3-text-black">Name</p><input class = "w3-input" type = "text" name = "name" > <br>
-						<p class="w3-text-black">Description</p>
-						<textarea rows="4" cols="50" name="description" form="usrform"></textarea>
-					</fieldset>
+			<div style="overflow:auto;height:70%">
+				<div class="w3-panel w3-grey" style="margin:10px;margin-left:26%">
 					<?php
-						if($addType== "assignment"){
-							echo "<fieldset style='margin:10px;margin-left:26%'>";
-							echo "<legend>Availability</legend>";
-							echo "<p>Allow submissions from </p>";
-							echo "<input class='w3-input' type = 'date'  name = 'dateStart' id = 'dateStart' disabled>";
-							echo "<input type = 'checkbox' name = 'dateStartCheck' id = 'dateStartCheck'>Enable<br>";
-							echo "<p>Due date </p>";
-							echo "<input class='w3-input' type = 'date'  name = 'dateDue' id = 'dateDue' disabled><br>";
-							echo "<input type = 'checkbox' name = 'dateDueCheck' id = 'dateDueCheck'>Enable<br>";
-							echo "</fieldset>";
-						}
+						echo "<p class='w3-xlarge w3-text-black'>Adding a new ".$_GET['addType']."</p>";
 					?>
-					<fieldset style="margin:10px;margin-left:26%">
-						<legend>Content</legend>
-						<p>Select files</p>
-						<input type = "file" name = "upfile" id = "upfile" style='margin:10px;margin-left:26%'>
-					</fieldset>
+				</div>
 
+				<div style="margin:10px;margin-left:25%">
+					<button class="w3-black w3-button w3-right w3-margin" id= "btnCollapseAll">Collapse All <i class="fa fa-caret-down" aria-hidden="true"></i></button>
+					<form id="usrform" method="post" enctype="multipart/form-data">
+						<div class="w3-container w3-margin-bottom">
+							<fieldset >	
+								<legend><button class="w3-button w3-black" id="btnGeneral">General <i class="fa fa-caret-down" aria-hidden="true"></i></button></legend>
+								<div class="w3-hide" id="fsGeneral">
+									<table class="w3-table w3-centered">
+									<tr>
+										<td><span style="color:red">Name *</span></td>
+										<td><input class = "" type = "text" name = "name" style="width:100%"></td>
+									</tr>
+									<tr>
+										<td><p class="w3-text-black">Description</p></td>
+										<td><textarea rows="4" cols="50" name="description" form="usrform" style="width:100%"></textarea></td>
+									</tr>
+								</table>
+								</div>
+							</fieldset>
+						</div>
+						
+						<?php
+							if($addType== "assignment"){
+								echo "<div class='w3-container w3-margin-bottom'>";
+								echo "<fieldset>";
+								echo "<legend><button class='w3-button w3-black' id='btnAvailibility'>Availability <i class='fa fa-caret-down' aria-hidden='true'></i></button></legend>";
+								echo "<div class='w3-hide' id='fsAvail'>";
+								echo "<table class='w3-centered'>";
+								echo "<tr>";
+								echo "<td>Allow submissions from <i class='fa fa-question-circle' aria-hidden='true'></i></td>";
+								echo "<td><input class='' type = 'date'  name = 'dateStart' id = 'dateStart' disabled></td>";
+								echo "<td><input type = 'checkbox' name = 'dateStartCheck' id = 'dateStartCheck'>Enable</td>";
+								echo "</tr>";
+								echo "<tr>";
+								echo "<td>Due date <i class='fa fa-question-circle' aria-hidden='true'></i></td>";
+								echo "<td><input class='' type = 'date'  name = 'dateDue' id = 'dateDue' disabled></td>";
+								echo "<td><input type = 'checkbox' name = 'dateDueCheck' id = 'dateDueCheck'>Enable</td>";
+								echo "</tr>";
+								echo "</table>";
+								echo "</div>";
+								echo "</fieldset>";
+								echo "</div>";
+							}
+						?>
+						<div class="w3-container w3-margin-bottom">
+							<fieldset>
+								<legend><button class="w3-button w3-black" id="btnContent" >Content <i class="fa fa-caret-down" aria-hidden="true"></i></button></legend>
+								<div class="w3-hide" id="fsContent">
+									Select files <i class='fa fa-question-circle' aria-hidden='true'></i>
+									<input type = "file" name = "upfile" id = "upfile" style='margin:10px;margin-left:26%'>
+								</div>
+							</fieldset>
+						</div>
 
-					<input class = "w3-btn" type = "submit" name = "save" value = "Save and Return to Course" style='margin:10px;margin-left:26%'>
-					<input class = "w3-btn" type = "submit" name = "cancel" value = "Cancel" style='margin:10px;margin-left:26%'>
-				</form>
-				<br>
-
+						<div class="w3-center">
+							<input class = "w3-btn" type = "submit" name = "save" value = "Save and Return to Course" >
+							<input class = "w3-btn" type = "submit" name = "cancel" value = "Cancel">
+						</div>
+					</form>
+					<br>
+				</div>
 			</div>
 		</div>
-
-
-	</div>
 	</body>
 
 	<script>
 	$(document).ready(function(){
-			$("input[name='dateStartCheck']").click(function(){
-				if($(this).is(':checked')){
-					$('#dateStart').prop("disabled", false);
-				}
-				else{
-					$('#dateStart').prop("disabled", true);
-				}
-			});
-			$("input[name='dateDueCheck']").click(function(){
-				if($(this).is(':checked')){
-					$('#dateDue').prop("disabled", false);
-				}
-				else{
-					$('#dateDue').prop("disabled", true);
-				}
-			});
-
+		$("input[name='dateStartCheck']").click(function(){
+			if($(this).is(':checked')){
+				$('#dateStart').prop("disabled", false);
+			}
+			else{
+				$('#dateStart').prop("disabled", true);
+			}
 		});
+		$("input[name='dateDueCheck']").click(function(){
+			if($(this).is(':checked')){
+				$('#dateDue').prop("disabled", false);
+			}
+			else{
+				$('#dateDue').prop("disabled", true);
+			}
+		});
+
+	});
+
+	$("#btnAvailibility").click(function(e){
+			e.preventDefault();
+			if($("#fsAvail").hasClass("w3-hide")){
+				$("#fsAvail").removeClass("w3-hide").addClass("w3-show");
+			} else{
+				$("#fsAvail").removeClass("w3-show").addClass("w3-hide");
+			}
+	});
+
+	$("#btnGeneral").click(function(e){
+			e.preventDefault();
+			if($("#fsGeneral").hasClass("w3-hide")){
+				$("#fsGeneral").removeClass("w3-hide").addClass("w3-show");
+			} else{
+				$("#fsGeneral").removeClass("w3-show").addClass("w3-hide");
+			}
+	});
+
+	$("#btnContent").click(function(e){
+			e.preventDefault();
+			if($("#fsContent").hasClass("w3-hide")){
+				$("#fsContent").removeClass("w3-hide").addClass("w3-show");
+			} else{
+				$("#fsContent").removeClass("w3-show").addClass("w3-hide");
+			}
+	});
+
+	$state = true;
+	$("#btnCollapseAll").click(function(e){
+			e.preventDefault();
+			
+			if($state == true){
+				$("#fsAvail").removeClass("w3-hide").addClass("w3-show");
+				$("#fsContent").removeClass("w3-hide").addClass("w3-show");
+				$("#fsGeneral").removeClass("w3-hide").addClass("w3-show");
+				$state = false;
+			} else{
+				$("#fsAvail").removeClass("w3-show").addClass("w3-hide");
+				$("#fsContent").removeClass("w3-show").addClass("w3-hide");
+				$("#fsGeneral").removeClass("w3-show").addClass("w3-hide");
+				$state = true;
+			}
+	});
 	</script>
 </html>
--- phpScript/courses.php
+++ phpScript/courses.php
@@ -1,25 +1,19 @@
 <?php 
+        include ('connection.php');
 
-include ('connection.php');
-
-$id=$_SESSION['id'];
-$query="SELECT CONCAT(Courses.code,'/',Courses.course) as course, Courses.ID_C as id
-        FROM Courses JOIN Enrollments
-        ON Courses.ID_C =Enrollments.ID_C
-        WHERE $id=Enrollments.ID_U";
-
-
-    
+        $id=$_SESSION['id'];
+        $query="SELECT CONCAT(Courses.code,'/',Courses.course) as course, Courses.ID_C as id
+                FROM Courses JOIN Enrollments
+                ON Courses.ID_C =Enrollments.ID_C
+                WHERE $id=Enrollments.ID_U";
 ?>
 
-<?php if($result=$conn->query($query)){
-        while($row=$result->fetch_array()){
-                echo "<div class='w3-card-2 w3-container w3-padding' style='margin:10px;margin-left:26%'>";
-                echo "<a href='course.php?id=".$row["id"]."&courseTitle=".$row["course"]."' class='w3-xlarge w3-text-black'> ".$row["course"];
-                echo "</div>";	
-                
-                			
+<?php 
+        if($result=$conn->query($query)){
+                while($row=$result->fetch_array()){
+                        echo "<div class='w3-card-2 w3-container w3-padding' style='margin:10px;margin-left:26%'>";
+                        echo "<a href='course.php?id=".$row["id"]."&courseTitle=".$row["course"]."' class='w3-xlarge w3-text-black' style='text-decoration:none'> ".$row["course"];
+                        echo "</div>";		
+                }   
         }
-        
-}
 ?>
